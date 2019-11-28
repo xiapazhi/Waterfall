5bcf03b74318d61443dca8f0b66024b7a6128a41
YUAN21 <1650192445@qq.com>
2019-11-28 17:30:55 +0800

    更改setup 尝试启动

--- setup.py
+++ setup.py
@@ -2,5 +2,14 @@ import logging
 import Flaskr
 import sys
 
-app = Flaskr.create_app(None)
-app.run()
+# app = Flaskr.create_app(None)
+# app.run()
+
+
+def app(environ, start_response):
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
