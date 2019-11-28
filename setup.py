8dda157247fbccf18d3521a69fb72813b50e90d8
YUAN21 <1650192445@qq.com>
2019-11-28 17:39:50 +0800

    尝试另一种方式

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ import logging
 import Flaskr
 import sys
 
-app = Flaskr.create_app(None)
+# app = Flaskr.create_app(None)
 # app.run()
 
 
-# def app(environ, start_response):
-#     data = b"Hello, World!\n"
-#     start_response("200 OK", [
-#         ("Content-Type", "text/plain"),
-#         ("Content-Length", str(len(data)))
-#     ])
-#     app = Flaskr.create_app(None)
-#     return app.run()
+def app(environ, start_response):
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    app = Flaskr.create_app(None)
+    return app.run()
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
