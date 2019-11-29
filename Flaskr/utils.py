7d34c04c8c1b7853c10d16a388a6ce31e3662905
YUAN21 <1650192445@qq.com>
2019-11-29 15:51:06 +0800

    按序上传 随机返回

--- Flaskr/utils.py
+++ Flaskr/utils.py
@@ -1,5 +1,6 @@
 from flask import current_app
 from . import db
+import random
 
 
 def allowed_file(filename):
@@ -10,14 +11,41 @@ def allowed_file(filename):
 
 def get_picture(already=[]):
     try:
-        # searchSql = 'SELECT * FROM picture WHERE id NOT IN ( 26,27,28)'
-        searchSql = f"SELECT * FROM picture"
-        if len(already) > 0:
-            alreadyStr = ','.join([str(id) for id in already])
-            searchSql = searchSql + " WHERE id NOT IN (" + alreadyStr + ")"
-        searchSql = searchSql + " LIMIT 15"
         sqlite = db.connect_db()
         cursor = sqlite.cursor()
+
+        searchMaxIdSql = 'SELECT MAX(id) FROM picture'
+        cursor.execute(searchMaxIdSql)
+        maxIdRaw = cursor.fetchone()
+        maxId = maxIdRaw[0]
+
+        searchAllSql = 'SELECT id FROM picture'
+        cursor.execute(searchAllSql)
+        allPicIdRaw = cursor.fetchall()
+        picIdList = []
+        for row in allPicIdRaw:
+            picIdList.append(row['id'])
+
+        diffNum = len(picIdList) - len(already)
+        realSearchIds = []
+        searchNum = 0
+
+        if diffNum > 0:
+            if diffNum > 30:
+                searchNum = 30
+            else:
+                searchNum = diffNum
+            while len(realSearchIds) < searchNum:
+                currRandonId = random.randint(1, maxId)
+                if picIdList.count(currRandonId) > 0 and already.count(currRandonId) == 0 and realSearchIds.count(currRandonId) == 0:
+                    realSearchIds.append(currRandonId)
+            # searchSql = 'SELECT * FROM picture WHERE id NOT IN ( 26,27,28)'
+        searchSql = f"SELECT * FROM picture"
+        if len(already) > 0:
+            realSearchIdStr = ','.join([str(id) for id in realSearchIds])
+            searchSql = searchSql + " WHERE id IN (" + realSearchIdStr + ")"
+        searchSql = searchSql + " LIMIT 15"
+
         cursor.execute(searchSql)
         return cursor.fetchall()
     except Exception as e:
